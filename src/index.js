16a30ca0dc493ddc628b6cee18fdd50590162f87
Rando Rostok <rrostok@malwarebytes.com>
2021-11-07 16:02:33 +0200

    basic intro scene

--- src/index.js
+++ src/index.js
@@ -5,22 +5,45 @@ import * as dat from 'dat.gui';
 
 const canvas = document.querySelector('canvas.webgl');
 
-// Debug
-const gui = new dat.GUI();
+const getBox = (h, w, d) => {
+  const geometry = new THREE.BoxGeometry(h, w, d);
+  const material = new THREE.MeshBasicMaterial({
+    color: 0x00ff00,
+  });
+  const mesh = new THREE.Mesh(geometry, material);
 
-// Scene
-const scene = new THREE.Scene();
+  return mesh;
+};
 
-// Camera
-const camera = new THREE.PerspectiveCamera(
-  45,
-  window.innerWidth/window.innerHeight,
-  1,
-  1000,
-);
+const init = () => {
+  // Debug
+  const gui = new dat.GUI();
+  
+  const scene = new THREE.Scene();
 
-// Renderer
-const renderer = new THREE.WebGL1Renderer({
-  canvas,
-});
-renderer.setSize(window.innerWidth, window.innerHeight);
+  const box = getBox(1, 1, 1);
+  scene.add(box);
+
+  const camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth/window.innerHeight,
+    1,
+    1000,
+  );
+  camera.position.x = 1;
+  camera.position.y = 2;
+  camera.position.z = 5;
+
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+  const renderer = new THREE.WebGL1Renderer({
+    canvas,
+    alpha: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  // Render
+  renderer.render(scene, camera);
+};
+
+init();
