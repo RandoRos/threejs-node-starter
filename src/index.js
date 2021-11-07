03553770b472bd429b88c7978f67f988567cd08a
Rando Rostok <rrostok@malwarebytes.com>
2021-11-07 16:25:29 +0200

    scene improvements

--- src/index.js
+++ src/index.js
@@ -5,6 +5,17 @@ import * as dat from 'dat.gui';
 
 const canvas = document.querySelector('canvas.webgl');
 
+const getPlane = (h) => {
+  const geometry = new THREE.PlaneBufferGeometry(h, h);
+  const material = new THREE.MeshBasicMaterial({
+    color: 0x93999b,
+    side: THREE.DoubleSide,
+  });
+  const mesh = new THREE.Mesh(geometry, material);
+
+  return mesh;
+};
+
 const getBox = (h, w, d) => {
   const geometry = new THREE.BoxGeometry(h, w, d);
   const material = new THREE.MeshBasicMaterial({
@@ -21,8 +32,16 @@ const init = () => {
   
   const scene = new THREE.Scene();
 
+  const plane = getPlane(3);
+  plane.rotation.x = Math.PI/2;
+  plane.position.x = 1.5;
+  
   const box = getBox(1, 1, 1);
+  box.position.y = box.geometry.parameters.height/2;
+  box.position.x = 1.5;
+
   scene.add(box);
+  scene.add(plane);
 
   const camera = new THREE.PerspectiveCamera(
     45,
