8baa347238e71e406c43b496661ae53eaf747b9c
Rando Rostok <rrostok@malwarebytes.com>
2021-11-07 14:51:00 +0200

    added three.js and dat.gui with basic setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import '../public/stylesheet.scss';
 
-const foo = () => 'Bar';
+import * as THREE from 'three';
+import * as dat from 'dat.gui';
 
-console.log('It`\'s working!');
-console.log('Fun ', foo());
+const canvas = document.querySelector('canvas.webgl');
+
+// Debug
+const gui = new dat.GUI();
+
+// Scene
+const scene = new THREE.Scene();
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth/window.innerHeight,
+  1,
+  1000,
+);
+
+// Renderer
+const renderer = new THREE.WebGL1Renderer({
+  canvas,
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
